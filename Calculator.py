b0b940a18f3401b53ce9606b1e70ba3d1bee873b
HerSlave <125888640+HerSlave@users.noreply.github.com>
2023-03-15 22:05:37 -0400

    Create Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,48 @@
+# This program creates a basic calculator
+
+# Define the addition function
+def add(x, y):
+    return x + y
+
+# Define the subtraction function
+def subtract(x, y):
+    return x - y
+
+# Define the multiplication function
+def multiply(x, y):
+    return x * y
+
+# Define the division function
+def divide(x, y):
+    return x / y
+
+# Main program loop
+while True:
+    print("Select operation.")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    # Take input from the user
+    choice = input("Enter choice (1/2/3/4): ")
+
+    # Check if choice is one of the four options
+    if choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        break
+    else:
+        print("Invalid Input")
